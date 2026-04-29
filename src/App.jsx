19bcc05095d62c1a7db09176bb7f59c0b5258c39
Mohit_tripathi <mohit.tripathi@teqfocus.com>
2026-04-29 21:20:14 +0530

    Re-add Resume page to stable single-theme branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import InteractiveBackground from './components/InteractiveBackground';
 import Navbar from './components/Navbar';
@@ -7,18 +7,29 @@ import Projects from './components/Projects';
 import Minigames from './components/Minigames';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
+import Resume from './components/Resume';
 
 function App() {
+  const [currentPage, setCurrentPage] = useState('home');
+
   return (
     <div className="app-container">
       <InteractiveBackground />
-      <Navbar />
-      <main>
-        <Hero />
-        <Projects />
-        <Minigames />
-        <Contact />
-      </main>
+      <Navbar currentPage={currentPage} setCurrentPage={setCurrentPage} />
+      
+      {currentPage === 'home' ? (
+        <main>
+          <Hero />
+          <Projects />
+          <Minigames />
+          <Contact />
+        </main>
+      ) : (
+        <main>
+          <Resume />
+        </main>
+      )}
+
       <Footer />
     </div>
   );
